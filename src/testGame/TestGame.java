3ccf228cc2fbc3bd6bd47d3d5af7fd5cf58146a5
Ege Apak <egeapak@hotmail.com>
2014-03-15 19:10:36 +0200

    Updated viewPort size

--- src/testGame/TestGame.java
+++ src/testGame/TestGame.java
@@ -18,7 +18,7 @@ public class TestGame implements ApplicationListener {
 	private Sprite bob;
 	private SpriteBatch BobBatch;
 	
-	private float speed = 0.1f; 					//We want it to move this amount every second.
+	private float speed = 5f; 					//We want it to move this amount every second.
 	private Vector2 direction = new Vector2(0,0); 	//Direction which we will move.
 	
 	@Override
@@ -26,7 +26,7 @@ public class TestGame implements ApplicationListener {
 		float w = Gdx.graphics.getWidth();
 		float h = Gdx.graphics.getHeight();
 		
-		camera = new OrthographicCamera(1f, h/w);
+		camera = new OrthographicCamera(w, h);
 		BobBatch = new SpriteBatch();
 		
 		camera.translate(camera.viewportWidth/2, camera.viewportHeight/2); 	//Moving the camera so (0,0) becomes lower left corner instead of center.
@@ -35,7 +35,7 @@ public class TestGame implements ApplicationListener {
 		//tex.setFilter(TextureFilter.Linear, TextureFilter.Linear);
 		
 		bob = new Sprite(tex);
-		bob.setSize(0.1f, 0.1f * bob.getHeight() / bob.getWidth());
+		bob.setSize(16f, 16f * bob.getHeight() / bob.getWidth());
 		bob.setOrigin(bob.getWidth()/2, bob.getHeight()/2);
 		bob.setPosition(bob.getX() - bob.getOriginX(), bob.getY() - bob.getOriginY());
 		
@@ -57,20 +57,10 @@ public class TestGame implements ApplicationListener {
 		
 		if(Gdx.input.isButtonPressed(Buttons.LEFT)) //If LEFT mouse button is pressed.
 		{
-			/*
-				Gdx.input.getY() returns the y coordinate of pointers location based on pixels but it is out of our boundary, which is between 0-1
-				Also it takes top-left corner as (0,0) so we subtract it from height to revert it and make bottom-left the (0,0) point.
-				We also multiply it with the ratio of our current camera boundaries and our windows's boundaries [camera.viewportHeight / Gdx.graphics.getHeight()]
-				We set the direction vector with these differences.
-				Then we check if te difference is greater than some value so it doesn't jump back and forth when it reaches the current mouse location
-				Then we normalize the vector, making its length 1
-				After that we multiplay it with our desired speed per second and deltaTime (time passed between last and current frame)
-				Finally, we move our guy.
-			*/
-			float diffx = (camera.viewportWidth / Gdx.graphics.getWidth()) * Gdx.input.getX() - bob.getX() - bob.getOriginX();
-			float diffy = (camera.viewportHeight / Gdx.graphics.getHeight()) * (Gdx.graphics.getHeight() - Gdx.input.getY()) - bob.getY() - bob.getOriginY();
+			float diffx = Gdx.input.getX() - bob.getX() - bob.getOriginX();
+			float diffy = (Gdx.graphics.getHeight() - Gdx.input.getY()) - bob.getY() - bob.getOriginY();
 			direction.set(diffx, diffy);
-			if (direction.len2() > 1/100000f)
+			if (direction.len2() > 1)
 			{
 				direction.nor();
 				direction.mul(speed * Gdx.graphics.getDeltaTime());
